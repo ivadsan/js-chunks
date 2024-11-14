6bc29930a4babbef22233e47eb7818400bf1251a
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2024-11-13 21:00:20 -0500

    destructuring update

--- src/destructuring/index.js
+++ src/destructuring/index.js
@@ -6,6 +6,7 @@ const foo = {
   e: "f",
   g: "h",
   i: "j",
+  k: "l",
 };
 
 const { a, c, e, ...bar } = foo;
@@ -24,7 +25,7 @@ i = 8;
 console.log(foo); // { a: 'b', c: 'd', e: 'f', g: 'x', i: 'j' }
 console.log(i); // 8
 
-let baz = { ...foo, a };
+let baz = { ...foo, a: 5 };
 baz.g = "y";
 console.log(baz); // { a: 'b', c: 'd', e: 'f', g: 'y', i: 'j' }
 console.log(foo); // { a: 'b', c: 'd', e: 'f', g: 'x', i: 'j' }
@@ -35,8 +36,20 @@ console.log(xyz); // { a: 'b', c: 'd', e: 'f', g: 'z', i: 'j' }
 console.log(foo); // { a: 'b', c: 'd', e: 'f', g: 'x', i: 'j' }
 
 //rename key
+console.log("rename key");
 const { a: z } = foo;
 console.log(z); // b
+console.log(foo);
 
 const test = { ...foo, a: 1, b: 2, c: 3 };
 console.log(test); // { a: 1, c: 3, e: 'f', g: 'x', i: 'j', b: 2 }
+
+const { k, ...sinK } = foo;
+const newK = { ...sinK };
+newK.otroK = k;
+console.log(k);
+console.log("newK", newK);
+
+// Version optimizada
+
+// const { k, ...newK } = { ...foo, otroK: foo.k };
